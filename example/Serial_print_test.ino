cd7093c3a9bb6edfa3aa0396405ea31075fa3deb
Michael lai <alphaseek@qq.com>
2020-11-27 11:38:01 +0800

    Create Serial_print_test.ino

---
+++ example/Serial_print_test.ino
@@ -0,0 +1,55 @@
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <MAX44009.h>
+
+#define SEALEVELPRESSURE_HPA (1018)
+
+Adafruit_BME280 bme; //  I2C Define BME280
+MAX44009 sensor;//I2C Define MAX44009
+
+unsigned long delayTime=3000;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("BME280 test"));
+
+  bool status;
+
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bme.begin(0x76);  
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+
+void loop() { 
+ // Output data to serial monitor
+  Serial.print("Ambient Light luminance :");
+  Serial.print(sensor.get_lux());
+  Serial.println(" lux");
+
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+
+  // Convert temperature to Fahrenheit
+  /*Serial.print("Temperature = ");
+  Serial.print(1.8 * bme.readTemperature() + 32);
+  Serial.println(" *F");*/
+  
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+  Serial.println();
+  delay(delayTime);
+}
